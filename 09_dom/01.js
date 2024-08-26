9564d3253f1345d269966989064ceae6514a483c
yasir <mohammadyasirkmt@gmail.com>
2024-08-26 16:10:49 +0530

    learning

---
+++ 09_dom/01.js
@@ -0,0 +1,91 @@
+// DOM 
+// document object model 
+
+// console.log(window.document);
+// console.dir(window.document);
+
+
+// events 
+// 1- click 
+// 2- submit 
+// 3- scroll 
+
+
+
+
+
+
+
+
+
+
+
+
+// elements that returns object
+// 1- document.getElementById('id') 
+// 2- document.querySelector('#id', '.class')
+// 3- document.querySelectorAll('.class')
+
+
+
+
+
+
+
+
+
+
+// change text 
+// textContent, innerText 
+
+// const header = document.getElementById('heading');
+// console.log(header.textContent); // give actual content if it's hidden
+// header.textContent = 'Schedule your tasks';
+
+// console.log(header.innerText); //give only visible content
+
+
+
+
+
+
+
+
+
+
+
+
+// change the style of elements 
+
+// const header = document.querySelector('.section-todo h2')
+// // console.log(header.style);
+// header.style.color = '#eee';
+// header.style.backgroundColor = '#333';
+
+
+
+
+
+
+
+
+
+
+
+// get and set attributes 
+
+// const inputElement = document.querySelector('.form-todo input');
+// console.log(inputElement.getAttribute('type'));
+
+// inputElement.setAttribute('placeholder', 'Add your tasks here');
+// console.log(inputElement.getAttribute('placeholder'));
+
+
+
+
+
+
+
+
+
+
