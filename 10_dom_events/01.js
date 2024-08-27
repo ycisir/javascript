6fabd5c4358e0f48f6e087d49203e2ea633df532
yasir <mohammadyasirkmt@gmail.com>
2024-08-27 20:20:02 +0530

    learning

---
+++ 10_dom_events/01.js
@@ -0,0 +1,32 @@
+// click event 
+
+// const firstBtn = document.querySelector('#one');
+// firstBtn.addEventListener('click', ()=> {
+//     console.log('clicked btn 1')
+// })
+
+
+// const allBtns = document.querySelectorAll('button');
+// console.log(allBtns)
+
+
+// arrow function cant work here 
+// for(let btn of allBtns) {
+//     btn.addEventListener('click', function() {
+//         console.log(this.textContent);
+//     })
+// }
+
+
+// for(let i=0; i<allBtns.length; i++) {
+//     allBtns[i].addEventListener('click', function() {
+//         console.log(this.textContent);
+//     })
+// }
+
+
+// allBtns.forEach((btn)=>{
+//     btn.addEventListener('click', function() {
+//         console.log(this.textContent);
+//     })
+// })
