5a9c0088c381fa35ae24add3c5e6faca15cf190a
yasir <mohammadyasirkmt@gmail.com>
2024-08-28 20:09:44 +0530

    learning

--- 11_asynchronous_js/callback.js
+++ 11_asynchronous_js/callback.js
@@ -62,7 +62,7 @@
 
 
 
-// pyramid of doom
+// pyramid of doom also callback hell
 
 const heading1 = document.querySelector('.heading1');
 const heading2 = document.querySelector('.heading2');
@@ -72,13 +72,34 @@ const heading5 = document.querySelector('.heading5');
 const heading6 = document.querySelector('.heading6');
 const heading7 = document.querySelector('.heading7');
 
-function changeText(element, text, color, time) {
+function changeText(element, text, color, time, onSuccess, onFailure) {
     setTimeout(()=>{
-        element.textContent = text;
-        element.style.color = color;
+        if(element) {
+            element.textContent = text;
+            element.style.color = color;
+
+            if(onSuccess){
+                onSuccess();
+            }
+        }else {
+            if(onFailure){
+                onFailure();
+            }
+        }
     }, time)
 }
 
-
-changeText(heading1, 'one', 'violet', 1000);
-changeText(heading2, 'two', 'purple', 2000);
+changeText(heading1, 'one', 'violet', 1000, ()=> {
+    changeText(heading2, 'two', 'purple', 2000, ()=> {
+        changeText(heading3, 'three', 'green', 3000, ()=> {
+            changeText(heading4, 'four', 'red', 4000, ()=> {
+                changeText(heading5, 'five', 'yellow', 5000, ()=> {
+                    changeText(heading6, 'six', 'blue', 6000, ()=> {
+                        changeText(heading7, 'seven', 'cyan', 7000, ()=> {
+                        }, ()=> {console.log('heading7 doesn\'t exist!');});
+                    }, ()=> {console.log('heading6 doesn\'t exist!');});
+                }, ()=> {console.log('heading5 doesn\'t exist!');});
+            }, ()=> {console.log('heading4 doesn\'t exist!');});
+        }, ()=> {console.log('heading3 doesn\'t exist!');});
+    }, ()=> {console.log('heading2 doesn\'t exist!');});
+}, ()=> {console.log('heading1 doesn\'t exist!');});
